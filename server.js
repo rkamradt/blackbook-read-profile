dfa292e0ee4000f20841c44b20213ee436e937d1
Randal Kamradt <randykamradt@gmail.com>
2021-02-05 16:18:20 -0800

    added getUser method

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+const MongoClient = require('mongodb').MongoClient
 import cors from 'cors'
 const OktaJwtVerifier = require('@okta/jwt-verifier')
 
@@ -8,6 +9,13 @@ const oktaJwtVerifier = new OktaJwtVerifier({
   issuer: 'https://dev-804011.okta.com/oauth2/default'
 });
 
+const MONGO_USER = process.env.MONGO_USER
+const MONGO_PASS = process.env.MONGO_PASS
+const MONGO_SERVER = process.env.MONGO_SERVER || 'mongodb-0'
+const MONGO_PORT = process.env.MONGO_PORT || 27017
+
+const dbName = 'blackbook'
+const mongourl = `mongodb://${MONGO_USER}:${MONGO_PASS}@${MONGO_SERVER}:${MONGO_PORT}`;
 /**
  * A simple middleware that asserts valid access tokens and sends 401 responses
  * if the token is not present or fails validation.  If the token is valid its
@@ -34,15 +42,62 @@ function authenticationRequired(req, res, next) {
     })
 }
 
+const client = new MongoClient(mongourl, {
+  useUnifiedTopology: true
+})
+
+async function getUser(req, res, next) {
+  const client = new MongoClient(mongourl, {
+    useUnifiedTopology: true
+  })
+
+  try {
+    const email = req.jwt.claims.sub
+    if(!email) {
+      console.log('no user in request')
+      return next()
+    }
+    console.log(`Connecting to ${mongourl}`)
+    await client.connect()
+    console.log('Connected correctly to server')
+
+    const db = client.db(dbName)
+    const query = { user: email };
+    var user = await db.collection('users').findOne(query);
+    if(!user) {
+      user = {
+        user: email,
+        firstName: '',
+        lastName: '',
+        privateData: 'sample private data'
+      }
+      const r = await db.collection('users').insertOne(user, {
+          w: 'majority',
+          wtimeout: 10000,
+          serializeFunctions: true,
+          forceServerObjectId: true
+        }
+      )
+    }
+
+    res.json(user)
+  } catch (err) {
+    client.close()
+    console.log(err.stack)
+  }
+
+  console.log('Closing mongoDB connection')
+  client.close()
+  console.log('Ending')
+}
+
 var app = express()
 
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.get('/users', authenticationRequired, (req, res) => {
-  res.json(req.jwt);
-})
+app.get('/users', authenticationRequired, getUser)
 
 const { PORT = 3000 } = process.env
 
